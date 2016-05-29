0232acd98f0dc8112470e7f6a7f87e86096ba132
Himanshu_Kattelu <Himanshukattelu@gmail.com>
2016-05-29 15:43:11 -0400

    Insert and remove queue methods

--- prioQueue.c
+++ prioQueue.c
@@ -10,10 +10,50 @@ struct prioQueue* createQueue(){
 
 }
 
+void insertQueue(struct huffman_char huffchar,struct prioQueue* priorityQueue){
+
+	if(priorityQueue->front == -1 || priorityQueue->front > priorityQueue->rear){
+
+		//If queue is empty, insert at the start
+		priorityQueue->huff_queue[0] = huffchar;
+		front = 0;
+		rear = 0;
+
+	} else {
+
+		//Go through the queue to find a spot to place the char
+		int index = front;
+		while(huffman_char.frequency < priorityQueue->huff_queue[index].frequency && index <= rear) index++;
+
+		int counter = index;
+		//Copy over the rest of the list, then insert the char
+		while(counter < QUEUE_MAX){
+			priorityQueue->huff_queue[counter+1] = priorityQueue->huff_queue[counter];
+			counter++;
+		}
+
+		//Set value and increment end index
+		priorityQueue->huff_queue[index] = huffchar;
+		rear++;
+
+	}
+
+	return;
+
+}
+
+struct huffman_char* removeQueue(struct prioQueue* priorityQueue){
+
+	if(priorityQueue->front == -1 || priorityQueue->front > priorityQueue->rear) return NULL;
+	
+	front++;
+	return priorityQueue->huff_queue[front-1];
+
+}
+
 void deleteQueue(struct prioQueue* priorityQueue){
 
 	memset(priorityQueue->huff_queue,0,QUEUE_MAX * sizeof(struct priorityQueue) + 2*sizeof(int));
 	free(priorityQueue);
-	
-}
 
+}
--- prioQueue.h
+++ prioQueue.h
@@ -1,4 +1,4 @@
-#define QUEUE_MAX 256
+#define QUEUE_MAX 300
 
 struct prioQueue{
 	struct huffman_char huff_queue[QUEUE_MAX];
@@ -13,7 +13,26 @@ Initializes a new priority queue object and returns a pointer to it.
 struct prioQueue* createQueue();
 
 /*
+Insert a specified huffman pairing into a priority queue.
+@param huffchar The pairing to insert
+@param priorityQueue The priority queue to insert the pairing into
+*/
+void insertQueue(struct huffman_char huffchar,struct prioQueue* priorityQueue);
+
+/*
+Pop an element off of a specified priority queue.
+@param priorityQueue The priority queue to pop
+@return a pointer to the popped pairing. NULL if the queue is empty.
+*/
+struct huffman_char* removeQueue(struct prioQueue* priorityQueue);
+
+/*
 Deletes a priority queue and free's its space from memory.
+@param priorityQueue the priority queue to delete
 */
 void deleteQueue(struct prioQueue* priorityQueue);
+
+
+
+
 
