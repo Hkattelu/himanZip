1d4106ebcc7a72e37bf3e3cc7983e37b304af3d9
Himanshu_Kattelu <Himanshukattelu@gmail.com>
2016-05-29 14:25:35 -0400

    createQueue() and deleteQueue()

--- prioQueue.c
+++ prioQueue.c
@@ -1,3 +1,19 @@
 #include "prioQueue.h"
 
+struct prioQueue* createQueue(){
+
+	struct prioQueue priorityQueue* = malloc(sizeof(struct priorityQueue));
+	memset(priorityQueue->huff_queue,0,QUEUE_MAX * sizeof(struct priorityQueue));
+	priorityQueue->front = -1;
+	priorityQueue->rear = -1;
+	return priorityQueue;
+
+}
+
+void deleteQueue(struct prioQueue* priorityQueue){
+
+	memset(priorityQueue->huff_queue,0,QUEUE_MAX * sizeof(struct priorityQueue) + 2*sizeof(int));
+	free(priorityQueue);
+	
+}
 
--- prioQueue.h
+++ prioQueue.h
@@ -1,6 +1,19 @@
-
-
 #define QUEUE_MAX 256
 
-int front = -1;
-int rear = -1;
+struct prioQueue{
+	struct huffman_char huff_queue[QUEUE_MAX];
+	int front;
+	int rear;
+};
+
+/*
+Initializes a new priority queue object and returns a pointer to it.
+@return Pointer to the newly created priority queue
+*/
+struct prioQueue* createQueue();
+
+/*
+Deletes a priority queue and free's its space from memory.
+*/
+void deleteQueue(struct prioQueue* priorityQueue);
+
